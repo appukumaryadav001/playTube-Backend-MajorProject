b15b1346baf2e3f0525c67bad6f77c424135c4ff
Appukumaryadav001 <appukumaryadav19@gmail.com>
2026-02-20 07:37:10 +0530

    add comment route

--- src/app.js
+++ src/app.js
@@ -20,9 +20,14 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/videos",videoRouter);
 app.use("/api/v1/subscriptions",subscriptionRouter);
+app.use("/api/v1/playlists",playlistRouter);
+app.use("/api/v1/comments",commentRouter);
+
 export {app};
---
+++ src/routes/comment.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+import {
+    getVideoComments,
+    updateComment,
+    addComment,
+    deleteComment
+} from "../controllers/comment.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+
+router.use(verifyJWT);
+
+router
+   .route("/:videoId")
+   .get(getVideoComments)
+   .post(addComment);
+
+router
+    .route("/comment/:commentId")
+    .patch(updateComment)
+    .delete(deleteComment);
+
+export default router;
